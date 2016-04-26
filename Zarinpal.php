eab5b945bdb55b9e7c84739dfc33ee77a8c6365b
Mahdi Majidzadeh <mahdi.majidzadeh@gmail.com>
2016-04-26 17:34:04 +0430

    change method

--- Zarinpal.php
+++ Zarinpal.php
@@ -1,34 +1,45 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-if ( !class_exists('nusoap_client')) 
+if (!class_exists('nusoap_client')) 
 	require_once('nusoap.php');
 
 class Zarinpal {
-    private $payment_meta;
-    private $payment_gate;
 
-    public function create( $merchant_id = '', $amount = 0, $gateway = 'ZarinGate' ) {
+	private $url;
+	private $ERR;
+	private $authority;
 
-        if ( $this->payment_meta || ! is_null( $this->payment_meta ) ) {
-            return false;
-        }
-        if ( $merchant_id == '' || strlen( $merchant_id ) != 16 ) {
-            return false;
-        }
-        if ( $amount <= 0 ) {
-            return false;
-        }
-        if ( ! $gateway == 'ZarinGate' || ! $gateway == 'WebGate' ) {
-            return false;
-        }
-        
-        $this->payment_meta = array(
-            'MerchantID'       =>  $merchant_id,
-            'Amount'            =>  $amount
-        );
-        $this->payment_gate = $gateway;
-        return true;
-    }
+	public function start($merchant_id , $amount, $desc, $call_back, $moble = NULL, $email = NULL){
+
+		$client = new nusoap_client('https://de.zarinpal.com/pg/services/WebGate/wsdl', 'wsdl'); 
+		$client->soap_defencoding = 'UTF-8';
+
+		$data = array(
+			'MerchantID' 	=> $merchant_id,
+			'Amount' 		=> $amount,
+			'Description' 	=> $desc,
+			'CallbackURL' 	=> $call_back
+			);
+		
+		if($phone){
+			$data['Mobile'] = $mobile;
+		}
+		if($email){
+			$data['Email'] = $email;
+		}	
+
+		$result = $client->call('PaymentRequest', array($data));
+
+		if($result['Status'] == 100){
+			$this->authority = $result['Authority'];
+			$this->url = 'https://www.zarinpal.com/pg/StartPay/'.$result['Authority'];
+			return TRUE;
+		}
+		else{
+			$this->ERR = $result['Status'];
+			return FALSE;
+		}
+	}
 
 }
